08c9f256d33014f39f948041a6150319952875e5
da-okazaki <da-okazaki@outlook.com>
2021-06-26 08:01:43 +0900

    [update] HomeScreen

--- src/components/pages/home-screen.tsx
+++ src/components/pages/home-screen.tsx
@@ -1,15 +1,26 @@
 import { FC } from "react";
-import { Grid, makeStyles, createStyles } from "@material-ui/core";
+import { Grid, Typography, makeStyles, createStyles } from "@material-ui/core";
 import { TopBar, ImageCard } from "../uikits";
 import { CardData } from "../../constants/card-data";
 const HomeScreen: FC = () => {
   const classes = useStyles();
   return (
-    <div>
-      <TopBar title="React for Tensorflow.js" />
-      <Grid container>
+    <div className={classes.root}>
+      <TopBar title="Tensorflow" />
+      <div className={classes.contentsTitle}>
+        <Typography variant="h5">モデル</Typography>
+        <Typography variant="subtitle2">
+          事前トレーニング済みの TensorFlow.js モデル
+        </Typography>
+      </div>
+      <Grid container className={classes.gridContainerStyle}>
         {CardData.map((item) => (
-          <Grid item xs={6} className={classes.gridStyle} key={item.id}>
+          <Grid
+            item
+            xs={6}
+            sm={4}
+            className={classes.gridItemStyle}
+            key={item.id}>
             <ImageCard
               image={item.image}
               title={item.title}
@@ -26,17 +37,37 @@ export default HomeScreen;
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {
-      width: "100vw",
-      display: "block",
-    },
-    title: {
-      flexGrow: 1,
+      height: "100%",
       display: "flex",
-      textAlign: "left",
-      marginLeft: theme.spacing(2),
-    },
-    gridStyle: {
+      alignItems: "center",
+      flexDirection: "column",
       padding: "8px",
+      [theme.breakpoints.down("xs")]: {
+        width: "100%",
+      },
+      [theme.breakpoints.up("sm")]: {
+        width: "65%",
+      },
+    },
+
+    contentsTitle: {
+      height: "100px",
+      width: "100%",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "start",
+      flexDirection: "column",
+      marginLeft: theme.spacing(2),
+      left: "20px",
+    },
+
+    gridContainerStyle: {
+      width: "100%",
+    },
+
+    gridItemStyle: {
+      padding: "8px",
+      width: "100%",
     },
   })
 );
