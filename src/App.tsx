da8b2775db2639e848dda4f3fc08ee44520ff6d2
da-okazaki <da-okazaki@outlook.com>
2021-06-26 09:23:58 +0900

    [update] App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import "./App.css";
+import { makeStyles, createStyles } from "@material-ui/core";
 import HomeScreen from "./components/pages/home-screen";
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
+    <div className={classes.app}>
       <HomeScreen />
     </div>
   );
 }
 
 export default App;
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    app: {
+      width: "100vw",
+      height: "calc(100vh - 48px)",
+      margin: "48px 0 0 0",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "start",
+    },
+  })
+);
